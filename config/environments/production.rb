5e293b652515a8ae63181a5959697ed067a247a2
pethl <pethicklisa@gmail.com>
2024-03-02 17:17:18 +0000

    prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,19 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options= {host: "https://netball-america-923def44b63e.herokuapp.com/"}
-  config.action_mailer.smtp_settings= {
-      address: ENV['MAILGUN_SMTP_SERVER'],
-      port: ['MAILGUN_SMTP_PORT'],
-      domain: ['MAILGUN_DOMAIN'],
-      user_name: ['MAILGUN_SMTP_LOGIN'],
-      password: ['MAILGUN_SMTP_PASSWORD'],
-      authentication: :plain,
-      enable_starttls_auto: true 
-}
- 
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'https://netball-america-923def44b63e.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'netball-america-923def44b63e.herokuapp.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
