568ecc0865cef4d1d84c0190de2fb21be207a208
pethl <pethicklisa@gmail.com>
2024-05-16 12:06:09 +0100

    removed approval process and restarted confirmable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where(approved: true)
+    @users = User.where.not(confirmed_at: nil)
     @users = @users.order(created_at: :asc)
+    @users_awaiting_confirmation = User.where(approved: false)
+    @users_awaiting_confirmation= @users_awaiting_confirmation.order(created_at: :asc)
     @users_awaiting_approval = User.where(approved: false)
     @users_awaiting_approval= @users_awaiting_approval.order(created_at: :asc)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # , :lockable, :timeoutable,  :confirmable and :omniauthable
+  # , :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :trackable
          
@@ -19,13 +19,13 @@ class User < ApplicationRecord
     "#{self.first_name} #{self.last_name}"
   end
   
-  def active_for_authentication? 
-    super && approved?
-  end 
+#  def active_for_authentication? 
+#    super && approved?
+#  end 
   
-  def inactive_message 
-    approved? ? super : :not_approved
-  end
+#  def inactive_message 
+#    approved? ? super : :not_approved
+#  end
   
   def send_admin_mail
     ApplicationMailer.new_user_waiting_for_approval(email).deliver
