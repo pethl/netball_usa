b3134d05b1a233cc0ac8f04b0e6935501416bb03
pethl <pethicklisa@gmail.com>
2024-06-06 11:51:47 +0100

    transfer nested people params

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -196,7 +196,7 @@ class TransfersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def transfer_params
-      params.require(:transfer).permit(:person_id, :event_id, :role, :check_in, :check_out, :room_type, :hotel_reservation, :share_volunteer, :arrival_airline, :arrival_flight, :arrival_time, :departure_airline, :departure_flight, :departure_time, :no_pick_up, :notes, :phone, :hotel_name, :pick_up_grouping, :pickup_type, :pickup_note, :departure_grouping, :departure_type, :departure_note, :t_shirt_size, :visa_type, :umpire_badge_level, :certification_date,  :headshot, :certification, :people{})
+      params.require(:transfer).permit(:person_id, :event_id, :role, :check_in, :check_out, :room_type, :hotel_reservation, :share_volunteer, :arrival_airline, :arrival_flight, :arrival_time, :departure_airline, :departure_flight, :departure_time, :no_pick_up, :notes, :phone, :hotel_name, :pick_up_grouping, :pickup_type, :pickup_note, :departure_grouping, :departure_type, :departure_note, :t_shirt_size, :visa_type, :umpire_badge_level, :certification_date,  :headshot, :certification, :people_attributes => [ :level, :phone, :address, :associated, :gender, :tshirt_size, :uniform_size, :certification, :certification_date] )
     end
 end
 
