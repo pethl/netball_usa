5d3269ba3776dc08e47b29740d22f4cce0662273
pethl <pethicklisa@gmail.com>
2024-06-08 13:54:25 +0100

    shrine file types

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,15 @@
 class ImageUploader < Shrine
   # plugins and uploading logic
-  ALLOWED_TYPES  = %w[image/jpeg image/png image/webp]
+  ALLOWED_TYPES  = %w[image/jpeg image/png image/webp image/tiff]
+  ALLOWED_EXTENSION = %w[jpg jpeg png webp tiff tif]
   MAX_SIZE       = 10*1024*1024 # 10 MB
   MAX_DIMENSIONS = [5000, 5000] # 5000x5000
+  
 
   Attacher.validate do
     validate_size 0..MAX_SIZE
     validate_mime_type ALLOWED_TYPES
+    validate_extension ALLOWED_EXTENSION
 
    # if validate_mime_type ALLOWED_TYPES
    #   validate_max_dimensions MAX_DIMENSIONS
