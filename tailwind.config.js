3d01ed45c1de42c3473b22bcf70257128e7697b4
pethl <pethicklisa@gmail.com>
2024-08-04 11:34:19 +0100

    put back gray color tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const colors = require('tailwindcss/colors')
 delete colors['lightBlue'];
 delete colors['warmGray'];
 delete colors['trueGray'];
-delete colors['coolGray'];
 delete colors['blueGray'];
 
 /** @type {import('tailwindcss').Config} */
