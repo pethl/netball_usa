e40badb83afb401b5a4cb8f0266e213d9b2381b5
pethl <pethicklisa@gmail.com>
2024-08-22 09:29:29 +0100

    clean compile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-require 'devise/mailer'
-require 'mailer/admin_mailer'
 # frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
@@ -11,6 +9,8 @@ require 'mailer/admin_mailer'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  require 'devise/mailer' 
+  #require 'mailer/admin_mailer'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
